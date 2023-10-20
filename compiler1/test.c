a303efdd1cc5d947101761a9fcecb0b98432f6dc
Amardeep <52241125+amardeep786@users.noreply.github.com>
2023-10-20 09:17:01 +0530

    Add files via upload

---
+++ compiler1/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num = 42;
+    printf("Hello, World!\n");
+
+    /* This is a comment */
+    
+    // This is another comment
+
+    char str[] = "This is a string";
+
+    /* Unclosed comment at the end of this line
+    return 0;
+}
